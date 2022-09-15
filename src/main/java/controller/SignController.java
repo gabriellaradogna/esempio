f42aec34911db6afac4e20bb1f0353f21a252ace
Ma-alz <107549577+Ma-alz@users.noreply.github.com>
2022-09-15 17:24:17 +0200

    struttura
    pagine index e login aggiunte, collegamento col database e l'archivio immagini

--- src/main/java/controller/SignController.java
+++ src/main/java/controller/SignController.java
@@ -1,5 +1,18 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/login")
 public class SignController {
+
+	@GetMapping
+	public String getPage(Model model) {
+		return "login";
+	}
 
 }
