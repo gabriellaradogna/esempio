1549fd1090adf7eef185a27e241a4ee11f4d8e14
gabriellaradogna <113359736+gabriellaradogna@users.noreply.github.com>
2022-09-16 17:11:55 +0200

    agg immagini e video

---
+++ src/main/webapp/static/javascript/javascript.js
@@ -0,0 +1,23 @@
+const nav_li1 = document.getElementById("nav_li1");
+const nav_li2 = document.getElementById("nav_li2");
+const nav_li3 = document.getElementById("nav_li3");
+const nav_li4 = document.getElementById("nav_li4");
+const nav_li5 = document.getElementById("nav_li5");
+
+nav_li1.addEventListener("mouseover",sfondo);
+nav_li1.addEventListener("mouseout", ripristinasfondo);
+nav_li2.addEventListener("mouseover",sfondo);
+nav_li2.addEventListener("mouseout", ripristinasfondo);
+nav_li3.addEventListener("mouseover",sfondo);
+nav_li3.addEventListener("mouseout", ripristinasfondo);
+nav_li4.addEventListener("mouseover",sfondo);
+nav_li4.addEventListener("mouseout", ripristinasfondo);
+nav_li5.addEventListener("mouseover",sfondo);
+nav_li5.addEventListener("mouseout", ripristinasfondo);
+function sfondo () {
+  this.style.backgroundColor = "#333333";
+}
+
+function ripristinasfondo () {
+  this.style.backgroundColor = "#000000";
+}
