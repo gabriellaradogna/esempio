753403a241901656f2f18580f691f71cf6ea3a91
Ma-alz <107549577+Ma-alz@users.noreply.github.com>
2022-09-21 09:25:59 +0200

    agg

--- src/main/java/controller/ContattiController.java
+++ src/main/java/controller/ContattiController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/home/contatti")
-public class ContattiControlle {
+public class ContattiController {
 	
 	@GetMapping
 	public String getPage(Model model) {
