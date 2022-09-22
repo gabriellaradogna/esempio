3f6197ae860e3da2a1abc7ef9de3835979d7eadf
Ma-alz <107549577+Ma-alz@users.noreply.github.com>
2022-09-22 14:20:46 +0200

    registrazione-utente

--- src/main/java/controller/SignController.java
+++ src/main/java/controller/SignController.java
@@ -4,10 +4,14 @@ package controller;
 
 import java.sql.Date;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -55,6 +59,15 @@ public class SignController {
 			)
 	{
 		return "userarea";
+	}
+
+	
+	@PostMapping
+	public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult result) 
+	{
+		
+		return null;
+		
 	}
 
 }
